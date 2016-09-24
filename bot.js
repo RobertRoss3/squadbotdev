bc20e41018aa12d9eb48d3d44f77a3dcd6b7d647
Robert <robertrross3@gmail.com>
2016-09-24 16:15:23 -0400

    change recursive response

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "hello";
+  botResponse = "hola";
 
   options = {
     hostname: 'api.groupme.com',
