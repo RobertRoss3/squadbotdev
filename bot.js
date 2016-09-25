4a8835ca156e9906e703a65788534a17a015a5a1
Robert <robertrross3@gmail.com>
2016-09-24 23:33:23 -0400

    fuck

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ function respond() {
   //   this.res.writeHead(200);
   //   this.res.end();
   // }
-  if(request.text == botRegex.test(request.text)) {
+  if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage("- Jamal Rogers");
     this.res.end();
