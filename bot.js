e4bbfe586f345667400093040f64cd041c629346
Robert <robertrross3@gmail.com>
2016-09-24 22:44:53 -0400

    test

--- bot.js
+++ bot.js
@@ -5,11 +5,11 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\hello$/;
+      botRegex = /damn/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage("- Jamal Rogers");
     this.res.end();
   } else {
     console.log("don't care");
@@ -18,10 +18,8 @@ function respond() {
   }
 }
 
-function postMessage() {
+function postMessage(botResponse) {
   var botResponse, options, body, botReq;
-
-  botResponse = "hola";
 
   options = {
     hostname: 'api.groupme.com',
