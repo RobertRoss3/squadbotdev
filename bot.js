7438b14a3707a2f44424528c08d4991712356184
Robert <robertrross3@gmail.com>
2016-09-25 22:40:16 -0400

    fixed

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /\sdamn\s/i;
+      botRegex = /damn/i;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
