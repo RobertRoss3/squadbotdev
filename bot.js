dc494bbc86acf5f9b46f11da8f74fc7898e5ed04
Robert <robertrross3@gmail.com>
2016-09-25 21:53:40 -0400

    word

--- bot.js
+++ bot.js
@@ -5,17 +5,8 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /damn/i;
+      botRegex = /\sdamn\s/i;
 
-  // if(request.text && botRegex.test(request.text)) {
-  //   this.res.writeHead(200);
-  //   postMessage("- Jamal Rogers");
-  //   this.res.end();
-  // } else {
-  //   console.log("don't care");
-  //   this.res.writeHead(200);
-  //   this.res.end();
-  // }
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage("- Jamal Rogers");
