b0ae8b2e54c83ac95c040c6a480c671a8dfb0587
Robert <robertrross3@gmail.com>
2016-09-29 23:44:32 -0400

    Complicated

--- bot.js
+++ bot.js
@@ -73,10 +73,10 @@ function respond() {
     } else {
       this.res.writeHead(200);
       response = ["What?",
-                  [["What is it, @" + userName + "?",'tag',[[12,(11+userName.length)],userIDNum]]],
+                  ["What is it, @" + userName + "?",'tag',[[12,(11+userName.length)],userIDNum]],
                   "Yes?", "I'm awake!", "How can I help?", "Huh?","You called?"];
       randomNumber = Math.floor(Math.random()*response.length);
-      postMessage(response[randomNumber]);
+      postMessage(response[randomNumber][0],response[randomNumber][1],[response[randomNumber[2]],response[randomNumber[3]]]);
       this.res.end();
     }
   } else {
