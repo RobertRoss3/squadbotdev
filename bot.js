c60f3b565f18d724537f2975f288c5ef25638667
Robert <robertrross3@gmail.com>
2016-10-02 19:03:29 -0400

    xml to JSON

--- bot.js
+++ bot.js
@@ -218,6 +218,7 @@ function getMath(equation) {
     });
 
     response.on('end', function() {
+      str = xmlToJson(str);
       if (!(str && JSON.parse(str).data[0])) {
         postMessage('Can\'t calculate that...');
       } else {
@@ -267,6 +268,45 @@ cleverBot.create(function (err, session) {
 
   // Woo, you initialized cleverbot.io.  Insert further code here
 });
+
+// Changes XML to JSON
+function xmlToJson(xml) {
+
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
 
 function postMessage(botResponse,type,args) {
   var botResponse, type, args, options, body, botReq;
