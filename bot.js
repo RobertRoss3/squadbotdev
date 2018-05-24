c2684378f324aa2d388d65676946ac9acd61904f
Robert <robertrross3@gmail.com>
2018-05-24 00:13:53 -0400

    test (v8)

--- bot.js
+++ bot.js
@@ -345,24 +345,24 @@ function respond() {
       if(attachments != null){
         if(attachments.type == 'mentions'){
           response = "";
-          // UserIDs = attachments.user_ids;
-          // likeMessage(request.id);
-          // for(id=0;id<UserIDs.length;id++){
-          //   // if(Member_id.includes(attachments.user_ids[id])){
-          //   //   thisName = Member_name[Member_id.indexOf(attachments.user_ids[id])];
-          //   // } else {
-          //   //   thisName = "";
-          //   // }
-          //   // stringstart = attachments.loci[id][0]+1; stringend = stringstart+attachments.loci[id][1]-1;
-          //   // response += request.text.substring(stringstart,stringend);
-          //   // response += " has the ID "+attachments.user_ids[id]+" and is ";
-          //   // if(thisName){
-          //   //     response += "listed as \""+thisName+"\".";
-          //   // } else {
-          //   //     response += "not listed."
-          //   // }
-          //   response += '\n';
-          // }
+          UserIDs = attachments.user_ids;
+          likeMessage(request.id);
+          for(id=0;id<UserIDs.length;id++){
+            // if(Member_id.includes(attachments.user_ids[id])){
+            //   thisName = Member_name[Member_id.indexOf(attachments.user_ids[id])];
+            // } else {
+            //   thisName = "";
+            // }
+            // stringstart = attachments.loci[id][0]+1; stringend = stringstart+attachments.loci[id][1]-1;
+            // response += request.text.substring(stringstart,stringend);
+            // response += " has the ID "+attachments.user_ids[id]+" and is ";
+            // if(thisName){
+            //     response += "listed as \""+thisName+"\".";
+            // } else {
+            //     response += "not listed."
+            // }
+            response += '\n';
+          }
           postMessage(response);
         } else {
           postMessage("You have to tag someone.");
@@ -760,14 +760,14 @@ function postMessage(botResponse,type,args) {
   };
   API.Messages.create(accessToken,groupID,options, function(err,res){
     if (!err) {
-    } else {console.log('POSTING FAILED: ERROR ' + JSON.stringify(err));}
+    } else {console.log('POSTING FAILED: ERROR ' + err);}
   });
 };
 
 function likeMessage(messageID) {
   API.Likes.create(accessToken,groupID,messageID, function(err,res) {
     if (!err) {
-    } else {console.log('LIKING FAILED: ERROR ' + JSON.stringify(err));}
+    } else {console.log('LIKING FAILED: ERROR ' + err);}
   });
 };
 
