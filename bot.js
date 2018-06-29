4e9f0bacee1d708e84ba840da8df833cf534914c
Ross <robert.ross00@delta.com>
2018-06-29 11:02:24 -0400

    fix (v2)

--- bot.js
+++ bot.js
@@ -11,36 +11,40 @@ var wolfClient = require('node-wolfram');
 var ImageService = require('groupme').ImageService;
 var Guid = require('guid');
 var GoogleSpreadsheet = require('google-spreadsheet');
+var async = require('async');
 // var NodeGeocoder = require('node-geocoder');
 
 //  GETTING DATA FROM GOOGLE SPREADSHEET
 var doc = new GoogleSpreadsheet('1QklJC4tgKBrdW_LxQ1O4TD_drZNxc0iz0nc53U-wL44');
 var sheet;
 
-//  AUTHENTICATES THE GOOGLE ACCOUNT
-var creds_json = {
-  client_email: 'squadbot@api-project-1099113201494.iam.gserviceaccount.com',
-  private_key: process.env.GOOGLE_PRIVATE_KEY
-}
-
-doc.useServiceAccountAuth(creds_json, function(err,info){
-  if(err){
-    console.log(err);
-  } else {
-    console.log("Authenticated to Google spreadsheet: "+info);
-  }
-});
-
-//  GETS INFORMATION ABOUT THE DOCUMENT AND WORKSHEET
-doc.getInfo(function(err, info) {
-  if (info != null){
-    console.log('Loaded document: '+info.title+'... ');
-    Members_info = info.worksheets[0]; Groups_info = info.worksheets[1]; Quotes_info = info.worksheets[2];
-
-    // GETS INFORMATION ABOUT THE GROUPS
+async.series([
+  //  AUTHENTICATES THE GOOGLE ACCOUNT
+  function setAuth(step) {
+    var creds_json = {
+      client_email: 'squadbot@api-project-1099113201494.iam.gserviceaccount.com',
+      private_key: process.env.GOOGLE_PRIVATE_KEY
+    }
+    doc.useServiceAccountAuth(creds_json, step);
+  },
+  //  GETS INFORMATION ABOUT THE DOCUMENT AND WORKSHEET
+  function getInfoAndWorksheets(step) {
+    doc.getInfo(function(err, info) {
+      if (info != null){
+        //Loads document info and creates arrays that will be used for tagging and quoting
+        console.log('Loaded document: '+info.title+'... ');
+        Members_info = info.worksheets[0]; Groups_info = info.worksheets[1]; Quotes_info = info.worksheets[2];
+        console.log('Sheet 1: \''+Members_info.title+'\' (ID: '+Members_info.id+'), Sheet 2: \''+Groups_info.title+'\' (ID: '+Groups_info.id+')...');
+        step();
+      } else {console.log("Error: Spreadsheet returned undefined.")}
+    });
+  },
+  // GETS INFORMATION ABOUT THE GROUPS
+  function getGroupInfo(step) {
     Groups_info.getCells({'min-row': 1,'max-row': 3,'min-col': 1,'max-col': 25,'return-empty': false},
     function(err, cells) {
       groupcount = cells.length/3;
+      console.log("Counted "+groupcount+" groups...");
       Group = []; Group_name = []; Group_regex = []; Group_response = [];
       for (i = 0; i < groupcount; i++){
         Group_name[i] = cells[i].value;
@@ -51,50 +55,65 @@ doc.getInfo(function(err, info) {
         Group_response[i] = tempResponse.split('_');
         Group[i] = [Group_name[i],Group_regex[i],Group_response[i], new Array()];
       }
-      console.log("Counted "+groupcount+" groups...");
+      step();
     });
-
-    //  GETS INFORMATION ABOUT THE MEMBERS
+  },
+  //  GETS INFORMATION ABOUT THE MEMBERS
+  function getMemberInfo(step) {
     Members_info.getCells({'min-row': 2,'max-row': 100,'min-col': 1,'max-col': 2,'return-empty': false},
     function(err, cells) {
       membercount = cells.length/2;
+      console.log("Counted "+membercount+" members...");
       Member = []; Member_name = []; Member_id = [];
       for (i = 0; i < membercount; i++){
           Member_id[i] = cells[(i*2)].value;
           Member_name[i] = cells[(i*2)+1].value;
           Member[i] = [Member_id[i], Member_name[i]];
       }
-      console.log("Counted "+membercount+" members...");
       Member_id.push('43525551'); Member_name.push('SquadBot'); Member.push(['43525551','Squadbot']);
+      step();
     });
-
-    //  GETS INFORMATION ABOUT THE MEMBERS IN A GROUP
+  },
+  //  GETS INFORMATION ABOUT THE MEMBERS IN A GROUP
+  function getGroupMembers(step){
     Groups_info.getCells({'min-row': 4,'max-row': (4+membercount),'min-col': 1,'max-col': groupcount,'return-empty': true},
     function(err, cells){
       subGroup = new Array(groupcount);
       for (j=0;j<groupcount;j++){
         subGroup[j] = new Array()
         for (i=0;i<membercount;i++){
-          if (cells[(groupcount*i)+j].value != ''){
-            subGroup[j].push(cells[(groupcount*i)+j].value);}}
+          if (cells[(groupcount*i)+j].value != ''){subGroup[j].push(cells[(groupcount*i)+j].value);}
+        }
         Group[j][3] = subGroup[j];
         for(k=0;k<Group[j][3].length;k++){
+          // if(Member_name.indexOf(Group[j][3][k])>-1){
           if(Member_name.includes(Group[j][3][k])){
             Group[j][3][k] = Member_id[Member_name.indexOf(Group[j][3][k])];
-          }}}
+          }
+        }
+        // console.log("Members of "+Group[j][0]+": "+Group[j][3]);
+      }
+      step();
     });
-
-    //  GETS QUOTES
+  },
+  //  GETS QUOTES
+  function getQuotes(step){
     Quotes_info.getCells({'min-row': 2,'max-row': 300,'min-col': 1,'max-col': 1,'return-empty': false},
     function(err, cells){
       quotecount = cells.length;
+      console.log("Counted "+quotecount+" quotes...");
       Quotes = [];
       for (i = 0; i < quotecount; i++){
           Quotes[i] = cells[i].value;
       }
-      console.log("Counted "+quotecount+" quotes...");
-    })
-  } else {console.log("Error: Spreadsheet returned undefined.")}
+      step();
+    });
+  },
+
+], function(err){
+    if( err ) {
+      console.log('Error: '+err);
+    }
 });
 
 console.log("Starting up...");
