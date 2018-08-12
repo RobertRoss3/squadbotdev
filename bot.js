f6c650c40ae93df6a05f12eaac9bdb30f5bfdb77
Robert <robertrross3@gmail.com>
2018-08-11 21:14:16 -0400

    squickfix

--- bot.js
+++ bot.js
@@ -731,7 +731,8 @@ function respond() {
         "giphy go away", "giphy leave", "you don't gotta go home, but you gotta get the fuck up outta here"];
         randomNumber = Math.floor(Math.random()*response2.length);
         if(/giphy/i.test(response2[randomNumber])){
-          response = response2.replace(/giphy/i, '');
+          response = response2[randomNumber]
+          response = response.replace(/giphy/i, '');
           searchGiphy(response);
         } else {
           response += response2[randomNumber];
